b4dd493553eac0bc07edf82588fcfbb6e8483a99
Priya <priya.mk@onixnet.us>
2023-03-16 13:17:36 +0530

    trigger now done

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use('/api/user',router)
 
 
 app.get('/',(req, res)=>{
-     res.send("Hello world node scriptingg to run  ")
+     res.send("Hello world node scripting  ")
 })
 
 // app.get('/get-all-products', async(req, res)=>{
