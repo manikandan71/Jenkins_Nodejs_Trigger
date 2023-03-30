69c1b45297355c92273e51796b6bd9142b69358f
Priya <priya.mk@onixnet.us>
2023-03-30 13:14:49 +0530

    new change

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.get('/',(req,res)=>{
-  res.send('hello from jenkins')
+  res.send('hello from jenkins testingss')
 })
 
 app.listen(5000,()=>{
